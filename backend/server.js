f6fb7e72363222e0c7dc7f359bdd709dfc8cff7c
Sonali-0 <166435812+Sonali-0@users.noreply.github.com>
2025-04-06 14:39:52 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://job-tracker-swart.vercel.app/',  
+  origin: ['https://job-tracker-swart.vercel.app/'],  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
