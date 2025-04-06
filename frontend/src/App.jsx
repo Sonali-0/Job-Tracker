22c6fc7396cb697c96fe65fc2bad23cdfa62ed3e
Sonali-0 <166435812+Sonali-0@users.noreply.github.com>
2025-04-06 19:00:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/ome";
+import Home from "./pages/Home";
 import JobForm from "./pages/JobForm"; 
 import EditJob from './pages/EditJob';
 import { ToastContainer } from 'react-toastify'; 
