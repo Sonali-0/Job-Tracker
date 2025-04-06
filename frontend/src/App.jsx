2a9520f80cd7a03d81e259887d00ac6e9de55450
Sonali-0 <166435812+Sonali-0@users.noreply.github.com>
2025-04-06 16:54:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,31 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import JobForm from "./pages/JobForm"; // Updated import path
+import JobForm from "./pages/JobForm"; 
+import { ToastContainer } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
 import "./styles.css";
 
 const App = () => {
   return (
     <Router>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add" element={<JobForm />} />
       </Routes>
+
+    
+      <ToastContainer 
+        position="top-center" 
+        autoClose={3000} 
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
     </Router>
   );
 };
