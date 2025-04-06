c1b46bf83c5713c88b4483f6c2a1f0737603feb5
Sonali-0 <166435812+Sonali-0@users.noreply.github.com>
2025-04-06 16:54:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,28 @@ const connectDatabase = require("./config/db.js");
 const jobRoutes = require("./routes/jobRoutes.js");
 const errorHandler = require("./middleware/errorHandler.js");
 
-// Load environment variables
+
 dotenv.config({ path: "./config/config.env" });
 
-// Connect to MongoDB
+
 connectDatabase();
 
 const app = express();
 
 // Middleware
 
+
 app.use(cors({
   origin: [
-    'http://localhost:5173' ,
-     'https://job-tracker-swart.vercel.app' 
+    'http://localhost:5173' 
+    ,'https://job-tracker-swart.vercel.app' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'], 
   credentials: true 
 }));
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -31,10 +34,11 @@ app.use(express.urlencoded({extended : true}));
 // Routes
 app.use("/api/jobs", jobRoutes);
 
-// Error handler 
+// Error handler (should come last)
 app.use(errorHandler);
 
 const PORT = process.env.PORT_VAR || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
