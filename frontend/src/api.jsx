c6d776ca0a06accedfa1796843a0c14084720253
Sonali-0 <166435812+Sonali-0@users.noreply.github.com>
2025-04-06 14:57:46 +0530

    api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,12 +1,10 @@
-// src/api.js
 import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://job-tracker-22ek.onrender.com/api/jobs',
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
+    "Content-Type": "application/json"
   }
 });
 
